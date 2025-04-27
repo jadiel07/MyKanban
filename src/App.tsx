b08aa696ecea6df40ebca814ad824d3770b0dfc1
jadiel07 <jadiel174@hotmail.com>
2025-04-26 23:53:14 -0300

    Add files

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Box, Heading } from "@radix-ui/themes"
+
+function App() {
+
+  return (
+    <>
+    <Box maxWidth="80rem" mx="auto">
+      <Heading>
+        React Kanban
+      </Heading>
+    </Box>
+    <Box>
+      <Heading>
+        Quadro de Tarefas
+      </Heading>
+    </Box>
+    </>
+  )
+}
+
+export default App
